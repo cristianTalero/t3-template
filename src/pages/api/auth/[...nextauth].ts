233fb26d8f85fcb5c969b156d3822ec04a25c550
Cristian Talero <kr15t14ng4mer@gmail.com>
2022-08-06 12:36:28 -0500

    Sentry handler added to API routes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,15 +2,24 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { PrismaClient } from '@prisma/client'
 import NextAuth from 'next-auth/next'
 import GoogleProvider from 'next-auth/providers/google'
+import { withSentry } from '@sentry/nextjs'
 
 const prisma = new PrismaClient()
 
-export default NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-})
+export const config = {
+  api: {
+    externalResolver: true,
+  },
+}
+
+export default withSentry(
+  NextAuth({
+    adapter: PrismaAdapter(prisma),
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      }),
+    ],
+  })
+)
--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,10 +1,18 @@
-// src/pages/api/trpc/[trpc].ts
 import { createNextApiHandler } from '@trpc/server/adapters/next'
 import { appRouter } from 'server/router'
 import { createContext } from 'server/router/context'
+import { withSentry } from '@sentry/nextjs'
+
+export const config = {
+  api: {
+    externalResolver: true,
+  },
+}
 
 // export API handler
-export default createNextApiHandler({
-  router: appRouter,
-  createContext: createContext,
-})
+export default withSentry(
+  createNextApiHandler({
+    router: appRouter,
+    createContext: createContext,
+  })
+)
