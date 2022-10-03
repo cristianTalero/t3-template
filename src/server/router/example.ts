5a1ddb979756c0e251edb6122d5c0f3f8e6c00b8
Cristian Talero <kr15t14ng4mer@gmail.com>
2022-10-02 21:36:36 -0500

    'next-connect' and 'formidable' added (trpc and next auth middleware pending)

--- src/server/router/example.ts
+++ src/server/router/example.ts
@@ -2,20 +2,20 @@ import { createRouter } from './context'
 import { z } from 'zod'
 
 export const exampleRouter = createRouter()
-  .query('hello', {
-    input: z
-      .object({
-        text: z.string().nullish(),
-      })
-      .nullish(),
-    resolve({ input }) {
-      return {
-        greeting: `Hello ${input?.text ?? 'world'}`,
-      }
-    },
-  })
-  .query('getAll', {
-    async resolve({ ctx }) {
-      return await ctx.prisma.user.findMany()
-    },
-  })
+	.query('hello', {
+		input: z
+			.object({
+				text: z.string().nullish(),
+			})
+			.nullish(),
+		resolve({ input }) {
+			return {
+				greeting: `Hello ${input?.text ?? 'world'}`,
+			}
+		},
+	})
+	.query('getAll', {
+		async resolve({ ctx }) {
+			return await ctx.prisma.user.findMany()
+		},
+	})
